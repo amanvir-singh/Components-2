b75277bf467acf241d9be542c0e247d572ab21e8
Amanvir <amanvir.singh880@gmail.com>
2024-02-20 19:33:01 -0500

    NavBar and ProductCard Updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
-export default function NavBar() {
+export default function NavBar({user}) {
   return (
     <>
       <a href="#home">Home </a>
       <a href="#home">Laptops </a>
       <a href="#contact">Contact </a>
       <a href="#about">About </a>
+      {(user !== undefined) ? <h3>Welcome {user.firstName} {user.lastName} </h3> : <h3>Login</h3>}
+      {(user.itemsInCart == 0) ? <h3></h3> : <h3> Items in Cart: {user.itemsInCart}</h3>}
     </>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,12 @@
-export default function ProductCard() {
+export default function ProductCard({product}) {
   return (
     <>
-      <img src="https://placehold.co/400x200.jpg?text=Product" alt="hp 15" />
-      <h3>
-        HP 15 Laptop w/ 1 year of Microsoft 365 - Natural Silver (Intel
-        N100/128GB SSD/4GB RAM)
-      </h3>
-      <p>
-        1.0GHz Intel N100 quad-core CPU and 4GB DDR4 RAM can power productivity
-        tasks as well as leisurely tasks including creating presentations,
-        watching videos, and casual photo editing 128GB UFS storage provides a
-        safe and reliable storage for your important documents and files
-      </p>
-      <div className="price">$487.79</div>
 
+      <img src={product.imageURL}/>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <div className="price">{product.price}</div>
+      {product.isInStock == false && <p>Unavailable</p>}
       <button>Add to Cart</button>
     </>
   );
