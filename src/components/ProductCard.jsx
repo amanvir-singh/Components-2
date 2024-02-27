f11b0a27a5c3f10adf052a69470b16d119cac741
Amanvir <amanvir.singh880@gmail.com>
2024-02-27 17:38:56 -0500

    Updated Product Card and CSS in Footer

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
 export default function ProductCard({product}) {
   return (
     <>
-
-      <img src={product.imageURL}/>
+      <br></br>
+      <img src={product.imageUrl}/>
       <h3>{product.name}</h3>
       <p>{product.description}</p>
       <div className="price">{product.price}</div>
       {product.isInStock == false && <p>Unavailable</p>}
-      <button>Add to Cart</button>
+      <button>Add to Cart</button><br></br>
     </>
   );
 }
--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -4,7 +4,7 @@ export default function ProductCardContainer({products}) {
   return (
     <>
       <h1>Discover Your Favorites</h1>
-      {products.map((product) => <ProductCard /> )}
+      {products.map((product) => <ProductCard key={product.name} product={product}/> )}
     </>
   );
 }
