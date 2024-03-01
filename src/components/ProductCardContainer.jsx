c7c5fb4186ffe7b1e801465f8029383875e924bf
Amanvir <amanvir.singh880@gmail.com>
2024-02-29 20:35:13 -0500

    Added Event Handler to Add to Cart

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,10 +1,12 @@
 import ProductCard from "./ProductCard";
 
 export default function ProductCardContainer({products}) {
+  
   return (
     <>
       <h1>Discover Your Favorites</h1>
-      {products.map((product) => <ProductCard key={product.name} product={product}/> )}
+      {products.map((product) => <ProductCard key={product.name} product={product} /> )}
     </>
   );
 }
+
